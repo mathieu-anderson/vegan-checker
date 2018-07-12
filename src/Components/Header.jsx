099682fc19f678ed748665955b516b3265d4d099
Mathieu <mathieu@woorank.com>
2018-07-12 14:54:26 +0200

    Add assets folder

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import reload from '../reload.svg';
+import reload from '../assets/reload.svg';
 import './Header.css';
 
 Header.propTypes = {
